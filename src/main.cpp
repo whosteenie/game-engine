aa8894c2928cb65af4b7db5c0336ffcf8aae6a68
Justin <justinmnmj@gmail.com>
2026-06-21 05:03:56 -0700

    render orange triangle using GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,61 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-
-#ifdef _WIN32
-#include <windows.h>
-#endif
-#include <GL/gl.h>
-
+#include <glad/glad.h>
 #include <iostream>
+
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    void main()
+    {
+        gl_Position = vec4(aPos, 1.0);
+    }
+    )";
+    const char* fragmentShaderSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main()
+    {
+        FragColor = vec4(0.9f, 0.4f, 0.2f, 1.0f);
+    }
+    )";
+
+unsigned int CompileShader(unsigned int type, const char* source)
+{
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+    int success;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        char log[512];
+        glGetShaderInfoLog(shader, 512, nullptr, log);
+        std::cerr << "Shader compile error:\n" << log << "\n";
+    }
+    return shader;
+}
+
+unsigned int CreateShaderProgram(const char* vertexSrc, const char* fragmentSrc)
+{
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexSrc);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentSrc);
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    int success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        char log[512];
+        glGetProgramInfoLog(program, 512, nullptr, log);
+        std::cerr << "Shader link error:\n" << log << "\n";
+    }
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
 
 int main()
 {
@@ -15,6 +64,10 @@ int main()
         std::cerr << "Failed to initialize GLFW\n";
         return 1;
     }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Game Engine", nullptr, nullptr);
     if (!window)
@@ -25,7 +78,30 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "Failed to initialize GLAD\n";
+        return 1;
+    }
+    
     glfwSwapInterval(1);
+
+    unsigned int shaderProgram = CreateShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,  // bottom-left
+         0.5f, -0.5f, 0.0f,  // bottom-right
+         0.0f,  0.5f, 0.0f   // top
+    };
+    unsigned int vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
 
     double lastFrameTime = glfwGetTime();
     
@@ -39,6 +115,9 @@ int main()
 
         glClearColor(0.08f, 0.09f, 0.15f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
 
         // Print FPS once per second
